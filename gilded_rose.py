1d778bb9ea24f5067d8c98240463ee235bc34583
emirsennin <n19238816@cs.hacettepe.edu.tr>
2024-07-18 21:24:18 +0300

    bugfix

--- gilded_rose.py
+++ gilded_rose.py
@@ -2,41 +2,6 @@
 from strategy_manager import AgedBrieStrategy, SulfurasStrategy, BackstagePassStrategy, ConjuredStrategy, NormalItemStrategy
 
 
-class GildedRoseOld:
-    def __init__(self, items):
-        self.items = items
-
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality -= 1
-            else:
-                if item.quality < 50:
-                    item.quality += 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality += 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality += 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in -= 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality -= 1
-                    else:
-                        item.quality = 0
-                else:
-                    if item.quality < 50:
-                        item.quality += 1
-
-
 class GildedRose(object):
 
     def __init__(self, items):
@@ -52,36 +17,6 @@ class GildedRose(object):
         for item in self.items:
             strategy = self.strategies.get(item.name, NormalItemStrategy())
             strategy.update(item)
-
-    def update_quality_old(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
 
 
 class Item:
